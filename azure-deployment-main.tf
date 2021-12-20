bdef2dd5ece6dea74d569afad973795e8865d22e
swdongko <68851760+swdongko@users.noreply.github.com>
2021-12-20 18:55:36 +0700

    Add files via upload

---
+++ azure-deployment-main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_app_service" "test" {
+  name                = "example-app-service"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
+
+
